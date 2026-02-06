517c115ebd80c26856c05aae314972d38290d6e1
muno17 <munozd17@gmail.com>
2026-02-06 15:16:11 -0500

    add functionality to add a sequence

--- api/save-sequence.php
+++ api/save-sequence.php
@@ -1,8 +1,36 @@
 <?php
 session_start();
 
+# If user is not logged in don't do anything
+if (!isset($_SESSION['logged_in'])) {
+    header("Location: /sqnc/sqnc.php");
+    exit;
+}
+
+
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
+$name = $data['name'];
 
+###*********### **** connect to db and insert **** ###*********### 
+try {
+    $host = "localhost";
+    $dbn = "sqnc_db";
+    $user = "root";
+    $pass = "";
 
+    $db = new PDO("mysql:host=$host;dbname=$dbn", $user, $pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    # id (auto), user_id, name, content, timestamp (auto)
+    $insertquery = $db->prepare("INSERT INTO sequences (user_id, name, content) 
+                                VALUES(?, ?, ?)");
+    $insertquery->execute(array($_SESSION['user_id'], $name, $json));
+
+} catch(PDOException $ex) {
+    # redirect with error
+    die("Database error: " . $ex->getMessage());
+    header("Location: /sqnc/sqnc.php");
+    exit;
+}
 ?>
